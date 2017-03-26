a580e0e932947ac63adf794e8de50b0171870f54
Tom Berghuis <thomas.berghuis@gmail.com>
2017-03-26 12:04:50 +0700

    .

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -7,9 +7,31 @@ import { observer, inject } from 'mobx-react';
 class Recipe extends Component {
 
     render() {
-        
+
+        let r = this.props.appState.recipes.filter((recipe) => {
+            return (this.props.params.id === recipe.id);
+        });
+
+
+        if (r.length < 1) {
+            return <div>invalid id</div>
+        }
+        let recipe = r[0];
+
         return (
-            <div>hello world</div>
+            <div class="recipe-details">
+                <div>Recipe Details</div>
+                <div>{recipe.title}</div>
+                <div class="c1">
+                    <div><img src={recipe.image} /></div>
+                    <div>
+                        <div>ingredients</div>
+                        <div>{recipe.ingredients.map((ingredient) => {
+                            return <div>{ingredient}</div>;
+                        })}</div>
+                    </div>
+                </div>
+            </div>
         );
     }
 
